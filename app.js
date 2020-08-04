4f84cfebc3465900aa0821dd3a6973ff61c93cb9
kattylucy <kattybarroso1321@gmail.com>
2020-08-04 13:46:02 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+///create server 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+    res.send('hello')
+})
+
+app.listen(8000);
+
